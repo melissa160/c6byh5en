70e34374747c6fe2993d0d4a838799bf109b8d53
melissa160 <melissa160@gmail.com>
2017-05-08 20:36:58 -0500

    corrigiendo para que pasen las pruebas

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::ProductsController < ApiController
 
   def index
     @products = Product.all
+
+    request.format = :json
+    respond_to do |format|
+      format.json { render :index }
+    end
   end
 
 end
