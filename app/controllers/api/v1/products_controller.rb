8da2a220bb401dd5c006d0af6bbdb73db3bd3e3a
melissa160 <melissa160@gmail.com>
2017-05-08 20:57:20 -0500

    usando request format

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,11 +2,6 @@ class Api::V1::ProductsController < ApiController
 
   def index
     @products = Product.all
-
-
-    respond_to do |format|
-      format.any { render :partial => 'api/v1/products/index.json.jbuilder'}
-    end
   end
 
 end
